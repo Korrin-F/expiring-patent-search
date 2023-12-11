1ea358d7b4281db06e1015f636da59d3a52fd24c
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2023-12-11 16:55:40 +0000

    add function to create rows for each search option

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -2,40 +2,68 @@
 
 // what year is it? 
 var currentYear = new Date().getFullYear();
+var mainSearchOption = "expiryYearRange";
+var optionalSearchOptions = ["keyword", "patentID", "applicant", "assignee", "inventor", "organization"]
+
 
 function loadLandingPage() {
     // updates the content in the Main Body section
     $('#landing').attr('class','show'); 
 
-    // search options cards
+    const container = $('#landing');
+
     // ---------------------------
-    //  Expiry date range selector
+    // SEARCH OPTIONS 
+    // ---------------------------
+
+    //  --- Expiry date range selector --- 
+    createDivRow(mainSearchOption);
     // Get the current year
     const currentYear = new Date().getFullYear();
-
-    // Function to create and append label and select tags
-    function createDropdown(labelText, dropdownId, startYear, endYear) {
-        const container = $('#landing');
-
-        // Create and append label
-        container.append(`<label for="${dropdownId}">${labelText}:</label>`);
-
-        // Create and append select
-        const select = $(`<select id="${dropdownId}"></select>`);
-        container.append(select);
-
-        // Populate select with a range of years
-        for (let year = startYear; year <= endYear; year++) {
-        select.append(`<option value="${year}">${year}</option>`);
-        }
-    }
-
     // Create and append start year dropdown with a range of years
     createDropdown('Start Year', 'startYear', currentYear-5, currentYear+5);
-
     // Create and append end year dropdown with a range of years
     createDropdown('End Year', 'endYear', currentYear-5, currentYear+5);
 
+    //  --- Optional Header ---
+    // Create and append header
+    createDivRow("sub-header");
+    var subHeaderDiv = $('#sub-header-row');
+    subHeaderDiv.append("<h3>Optional</h3>");
+
+    //  --- Rows for Optional Options ---
+    for(var item in optionalSearchOptions){
+        createDivRow(optionalSearchOptions[item]);
+    }
+}
+
+// --- EXPIRY YEAR RANGE SELECTOR ---
+// Function to create and append label and select tags
+function createDropdown(labelText, dropdownId, startYear, endYear) {
+    const container = $('#landing');
+
+    // Create and append label
+    container.append(`<label for="${dropdownId}">${labelText}:</label>`);
+
+    // Create and append select
+    const select = $(`<select id="${dropdownId}"></select>`);
+    container.append(select);
+
+    // Populate select with a range of years
+    for (let year = startYear; year <= endYear; year++) {
+    select.append(`<option value="${year}">${year}</option>`);
+    }
+}
+
+function createDivRow(item) {
+    // create a row div for each search option and the headers 
+    // append each row div to the container
+    const container = $('#landing');
+    // add the word row at the end of each item in the array
+    item += "-row";
+    // create a div with the id of the item in the array
+    container.append(`<div id="${item}" class="row"></div>`); 
+}
 
 
 
@@ -49,7 +77,7 @@ function loadLandingPage() {
     // 4. search by assignee
     
 
-}
+
 
 // ------------------------------
 // Collect inputs and do stuff when search buttons are clicked 
